f487544fbed7185e57ae3cd6fe46ecb5b505b493
Thomas <tj.collins1117@gmail.com>
2024-09-20 13:01:03 -0700

    last test

--- backend/index.js
+++ backend/index.js
@@ -21,6 +21,7 @@ app.use(cors({
       'http://localhost:5173',
       'https://gamespective.onrender.com',
       'https://gamespective.loca.lt',
+      'https://gamespective.loca.lt/',
       'https://gamespectivebackend.onrender.com/',
       'http://127.0.0.1:5173',
       'http://localhost:5000',
